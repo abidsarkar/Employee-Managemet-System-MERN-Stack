05167be97d6636412aedfd75ad730be4b0831411
abidsarkar <abidsarkar0@gmail.com>
2025-01-28 12:21:06 +0600

    Intial commit for ems react app

---
+++ README.md
@@ -0,0 +1,2 @@
+# Employee Management System
+This is a react app about EMS or employee management System
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <p className='' >my name is abid</p>  
+    </>
+  )
+}
+
+export default App
