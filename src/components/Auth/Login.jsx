4066029a2a11afbadb89fc18e6ed71ecf0b75bda
abidsarkar <abidsarkar0@gmail.com>
2025-02-02 20:28:23 +0600

    login page with set value and prevent default not useContext added

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,21 +1,47 @@
-import React from "react";
+// making 1st draft for desktop and dark them
+import React, { useState } from "react";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const submitHandle = (e) => {
+    e.preventDefault();
+    // console.log("email :", email);
+    // console.log("password :", password);
+    setEmail("");
+    setPassword("");
+  };
   return (
     <div className="h-screen w-screen  flex items-center justify-center">
       <div className="border-2  border-emerald-600 p-20 ">
-        <form className="flex flex-col items-center justify-center">
+        <form
+          className="flex flex-col items-center justify-center"
+          onSubmit={(e) => {
+            submitHandle(e);
+          }}
+        >
           <input
-            className="outline-none bg-transparent placeholder:text-gray-300 dark:placeholder:text-white text-black dark:text-white border-2 border-emerald-600 rounded-full py-3 px-5 text-xl"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+              // console.log('Chaining email');
+            }}
+            className="outline-none bg-transparent placeholder:text-gray-400  text-white border-2 border-emerald-600 rounded-full py-3 px-5 text-xl"
             type="email"
             placeholder="Enter your email"
+            required
           />
           <input
-            className="mt-3 outline-none bg-transparent placeholder:text-black dark:placeholder:text-white text-black dark:text-white border-2 border-emerald-600 rounded-full py-3 px-5 text-xl"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            className="mt-3 outline-none bg-transparent placeholder:text-gray-400 text-white border-2 border-emerald-600 rounded-full py-3 px-5 text-xl"
             type="password"
             placeholder="enter your password"
+            required
           />
-          <button className="mt-5 outline-none text-black dark:text-white border-none bg-emerald-600 rounded-full py-3 px-5 text-xl">
+          <button className="cursor-pointer mt-5 outline-none text-white border-none bg-emerald-600 rounded-full py-3 px-5 text-xl">
             Login
           </button>
         </form>
