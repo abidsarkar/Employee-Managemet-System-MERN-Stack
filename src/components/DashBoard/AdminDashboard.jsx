d5eef44ee8c20353ccf9bddeed3e02159605ee36
abidsarkar <abidsarkar0@gmail.com>
2025-02-03 15:48:56 +0600

    all task showing in admin dashboard

--- src/components/DashBoard/AdminDashboard.jsx
+++ src/components/DashBoard/AdminDashboard.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "../Other/Header";
 import CreateTask from "../Other/CreateTask";
+import AllTask from "../Other/AllTask";
 
 const AdminDashboard = () => {
   return (
     <div className="h-screen w-full p-10  text-gray-900">
       <Header />
       <CreateTask/>
+      <AllTask/>
     </div>
   );
 };
