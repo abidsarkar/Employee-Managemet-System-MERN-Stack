c39e464acaf2f56c7fe15ac47f9b4ffa5795defa
abidsarkar <abidsarkar0@gmail.com>
2025-02-20 20:41:24 +0600

    Implement localStorage setup for employees and admin on app load

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
-
-import './App.css'
-import Login from './components/Auth/Login'
-import AdminDashboard from './components/DashBoard/AdminDashboard'
-import EmployeeDashboard from './components/DashBoard/EmployeeDashboard'
+import { useEffect } from "react";
+import "./App.css";
+import Login from "./components/Auth/Login";
+import AdminDashboard from "./components/DashBoard/AdminDashboard";
+import EmployeeDashboard from "./components/DashBoard/EmployeeDashboard";
+import { setLocalStorage } from "./utils/LocalStroage";
 
 function App() {
-
+  useEffect(() => {
+    setLocalStorage( "employees","admin");
+  }, []);
 
   return (
     <>
-      <Login/> 
+      <Login />
       {/* <EmployeeDashboard/> */}
       {/* <AdminDashboard/> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
